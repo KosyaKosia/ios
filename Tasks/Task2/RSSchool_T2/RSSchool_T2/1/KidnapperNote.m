e629c8344ac401d0f538c6f5d1300bc59a5b6e63
KosyaKosia <dasha-kosyakova@mail.ru>
2019-04-01 23:44:27 +0300

    task2

--- Tasks/Task2/RSSchool_T2/RSSchool_T2/1/KidnapperNote.m
+++ Tasks/Task2/RSSchool_T2/RSSchool_T2/1/KidnapperNote.m
@@ -1,5 +1,24 @@
 #import "KidnapperNote.h"
 
 @implementation KidnapperNote
-// your code here
+
+- (BOOL)checkMagazine:(NSString *)magaine note:(NSString *)note {
+    
+    NSString* lowerCaseMAgazine = [magaine lowercaseString];
+    
+    NSArray* componentsFromMagazine = [lowerCaseMAgazine componentsSeparatedByString:@" "];
+    
+    NSCountedSet* setForMagazine = [[[NSCountedSet alloc] initWithArray:componentsFromMagazine] autorelease];
+    
+
+    NSString* lowerCaseNote = [note lowercaseString];
+    
+    NSArray* componentsFromNote = [lowerCaseNote componentsSeparatedByString:@" "];
+    
+    NSCountedSet* setForNote = [[[NSCountedSet alloc] initWithArray:componentsFromNote] autorelease];
+    
+    return [setForNote isSubsetOfSet:setForMagazine];
+    
+}
+
 @end
